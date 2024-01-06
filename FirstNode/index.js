21a4583743d10c663ead2e7dc3eeba5bcee89c3f
mishuk09 <mishukinfo09@gmail.com>
2024-01-06 12:38:40 +0530

    create node and fetch data

---
+++ FirstNode/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "  Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+]
+
+app.get('/users', (req, res) => {
+    console.log(req.params);
+    res.send("find user");
+})
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    const user = users[id];
+    res.send(user);
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello Node JAVASCRIPT");
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port:', port);
+});
